fdf50783946c885352db1d410cbc3a73c729eb39
smoaaza <smoaaza@mun.ca>
2025-09-22 18:24:30 -0230

    initial commit
    Added basic landing page with login, registration and ui

---
+++ frontend/src/components/index.js
@@ -0,0 +1,5 @@
+// Export all components from a single file for cleaner imports
+export { default as Header } from './Header';
+export { default as Main } from './Main';
+export { default as Footer } from './Footer';
+export { default as LoginModal } from './LoginModal';
