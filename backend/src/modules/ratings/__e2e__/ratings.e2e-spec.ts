ea43ab01860c717a9f372ab598f68f69ad7f4666
Aditi sharma <aditi.sharma110802@gmail.com>
2025-12-01 04:33:58 -0330

    Fixed e2e files

--- backend/src/modules/ratings/__e2e__/ratings.e2e-spec.ts
+++ backend/src/modules/ratings/__e2e__/ratings.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from 'src/app.module';
+import { AppModule } from '../../../app.module';
 
 describe('Ratings E2E', () => {
   let app: INestApplication;
